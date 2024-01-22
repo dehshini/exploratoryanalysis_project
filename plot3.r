1d0f3c97e56074e70b7fc2ab2cabdbc26d905b1e
dehshini <alidehshini@outlook.com>
2024-01-21 22:53:01 -0500

    plot3 code and image

--- plot3.r
+++ plot3.r
@@ -5,3 +5,33 @@
 #  Which have seen increases in emissions from 1999–2008? 
 #  Use the ggplot2 plotting system to make a plot answer this question.
 
+setwd("/Users/dehshini/code/R/exploratory_project")
+library(tidyverse)
+library(magrittr)
+
+# Read data
+NEI <- readRDS("./data/summarySCC_PM25.rds")
+SCC <- readRDS("./data/Source_Classification_Code.rds")
+
+#subset baltimore city
+baltimore <- NEI %>%
+    filter(fips == "24510")
+
+#group by year and source type
+baltimore_sources <- baltimore %>% 
+    group_by(year, type) %>% 
+    summarise(Emissions = sum(Emissions))
+
+#plot
+g <- ggplot(baltimore_sources, aes(year, Emissions, color = type)) +
+    geom_point() +
+    geom_line() +
+    ggtitle("Emissions from 1999-2008 in Baltimore City") +
+    xlab("Year") +
+    ylab("Emissions (tons)") +
+    theme_minimal()
+
+g
+
+#save
+dev.print(png, file = "plot3.png", width = 555, height = 455)
