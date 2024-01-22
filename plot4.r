0b6c6875a558035a629c6eacdb855cfcf5b1b05b
dehshini <alidehshini@outlook.com>
2024-01-21 23:16:59 -0500

    plot4 code and image

--- plot4.r
+++ plot4.r
@@ -8,3 +8,31 @@ library(magrittr)
 # Read data
 NEI <- readRDS("./data/summarySCC_PM25.rds")
 SCC <- readRDS("./data/Source_Classification_Code.rds")
+
+# Filter data
+coal <- SCC %>%
+    filter(
+        grepl(
+            "coal",
+            Short.Name,
+            ignore.case = TRUE
+        )
+    ) %>% 
+    select(SCC)
+
+NEI_coal <- NEI %>%
+    filter(SCC %in% coal$SCC)
+
+
+# Plot
+g <- ggplot(NEI_coal, aes(factor(year), Emissions, fill = type)) +
+    geom_bar(stat = "identity") +
+    ggtitle("Emissions from coal sources 1999-2008") +
+    xlab("year") +
+    ylab("Emissions") +
+    theme_minimal()
+
+g
+
+# Save plot
+dev.print(png, file = "plot4.png", width = 480, height = 480)
